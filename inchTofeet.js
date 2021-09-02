7d96cbc073929621f026faa223d1ff13baa6cc75
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-02 23:08:45 +0600

    upload

---
+++ inchTofeet.js
@@ -0,0 +1,10 @@
+function inchTofeet(inch){
+var feet = inch/12;
+return feet;
+}
+var nanafeet = inchTofeet(156);
+console.log(nanafeet);
+var nanifeet = inchTofeet(288);
+console.log(nanifeet);
+var dadifeet = inchTofeet(300);
+console.log(dadifeet);
