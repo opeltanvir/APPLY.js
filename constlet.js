6299fdac05931677dbbb26faf8c8bf2025cf2e29
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-02 23:24:27 +0600

    upload

---
+++ constlet.js
@@ -0,0 +1,10 @@
+var name = 'javed';
+if(name.lenth > 4){
+    name = 'juvu'
+}
+console.log(name);
+let age = 15;
+aee = 16;
+console.log(age);
+const country = 'bangladesh';
+console.log(country);
